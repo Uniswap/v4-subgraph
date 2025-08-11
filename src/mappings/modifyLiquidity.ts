b419ba13d81892528568f9978720d53532f99b1d
Crishna Iyengar <ciyengar3@gmail.com>
2025-08-11 11:20:54 -0400

    revert

--- src/mappings/modifyLiquidity.ts
+++ src/mappings/modifyLiquidity.ts
@@ -24,7 +24,6 @@ export function handleModifyLiquidityHelper(
   event: ModifyLiquidityEvent,
   subgraphConfig: SubgraphConfig = getSubgraphConfig(),
 ): void {
-  const start = Date.now()
   const poolManagerAddress = subgraphConfig.poolManagerAddress
 
   const bundle = Bundle.load('1')!
@@ -44,8 +43,6 @@ export function handleModifyLiquidityHelper(
 
   const token0 = Token.load(pool.token0)
   const token1 = Token.load(pool.token1)
-  const endLoadingFromDb = Date.now()
-  log.debug('handleModifyLiquidityDatabaseLoading time: {}', [endLoadingFromDb - start + ' ms'])
 
   if (token0 && token1) {
     const currTick = pool.tick!.toI32()
@@ -151,8 +148,6 @@ export function handleModifyLiquidityHelper(
     lowerTick.liquidityNet = lowerTick.liquidityNet.plus(amount)
     upperTick.liquidityGross = upperTick.liquidityGross.plus(amount)
     upperTick.liquidityNet = upperTick.liquidityNet.minus(amount)
-    const endCompute = Date.now()
-    log.debug('compute time: {}', [endCompute - endLoadingFromDb + ' ms'])
 
     lowerTick.save()
     upperTick.save()
@@ -170,6 +165,5 @@ export function handleModifyLiquidityHelper(
     pool.save()
     poolManager.save()
     modifyLiquidity.save()
-    log.debug('save time: {}', [Date.now() - endCompute + ' ms'])
   }
 }
